c3f27235ab17b181edf1f9f3c5d923de5d1e40a7
analuizabirck <analuizabirck2011@gmail.com>
2026-09-10 10:58:54 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-window.alert("É possível que o site não seja compatível com a proporção do desktop");
+window.alert("É possível que a exibição não seja compatível com a resolução do seu desktop");
 
 function selecionar() {
 }
